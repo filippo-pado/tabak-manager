2ce707661cfa2329983f0bccbb4a345adc4a337e
Unknown <filippo.pado2@gmail.com>
2018-02-08 11:46:20 +0100

    Backbone for rid verified

---
+++ src/app/rids/rid-verifier/rid-verifier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rid-verifier',
+  templateUrl: './rid-verifier.component.html',
+  styleUrls: ['./rid-verifier.component.css']
+})
+export class RidVerifierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

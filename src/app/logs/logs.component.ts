24bd42e1713b1316d03d95632de9a3757d5b580b
Unknown <filippo.pado2@gmail.com>
2018-02-15 11:46:45 +0100

    Prettify logs

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatSort } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
 
 import { Log } from './log';
 import { LogService } from '@app/core';
@@ -9,29 +8,14 @@ import { LogService } from '@app/core';
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.css']
 })
-export class LogsComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['date', 'requestType', 'body'];
-  dataSource: MatTableDataSource<Log>;
-
-  @ViewChild(MatSort) sort: MatSort;
+export class LogsComponent implements OnInit {
+  logs: Log[];
 
   constructor(private logService: LogService) { }
 
   ngOnInit() {
-    this.dataSource = new MatTableDataSource();
     this.logService.query({}, '', { date: 'desc' }, 100).then(logs => {
-      this.dataSource.data = logs;
+      this.logs = logs;
     });
   }
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
-
 }
